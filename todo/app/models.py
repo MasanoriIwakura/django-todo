5999fbe9837772af3b4b9d52b74a3a1e622b0c7c
MasanoriIwakura <hagobutler@yahoo.co.jp>
2019-01-14 22:45:03 +0900

    Add Todo App

--- todo/app/models.py
+++ todo/app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    body = models.CharField(max_length=100)
+    updated_at = models.DateTimeField(auto_now=True)
+
--- todo/app/views.py
+++ todo/app/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import Todo
+
+
+def index(request):
+    todo_list = Todo.objects.order_by('-updated_at')[:5]
+    template = loader.get_template('todo/index.html')
+    context = {
+        'todo_list': todo_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, id):
+    try:
+        todo = Todo.objects.get(pk=id)
+        template = loader.get_template('todo/detail.html')
+        context = {
+            'todo': todo,
+        }
+        return HttpResponse(template.render(context, request))
+    except Exception as e:
+        context = {
+            'msg': 'Error.'
+        }
+        return HttpResponse(template.render(context, request))
